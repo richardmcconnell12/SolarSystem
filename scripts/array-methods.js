3d2a8ee2ab58a2335ab545112047da2c67b7f1f8
Ricky McConnell <mr.mcconnell12@yahoo.com>
2019-04-30 19:43:18 -0500

    completed exercise -- added comments

--- scripts/array-methods.js
+++ scripts/array-methods.js
@@ -1,8 +1,9 @@
 const planetEl = document.getElementById("planets")
 const planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"]
 
+// Run a forEach method on the planets to log pag and append each planet to the DOM.
 planets.forEach(planet => {
-
+    // innerHTML to append each planet
     planetHtml = `<h3>${planet}</h3>
                 <hr>`
 
@@ -10,6 +11,8 @@ planets.forEach(planet => {
 });
 
 let newPlanetArray = [];
+
+// The map method to create a new array with toUpperCase method to capitalize the first character of each element.
 const planetArray = planets.map(planet => {
 
     newPlanetArray.push(planet.charAt(0).toUpperCase() + planet.substr(1));
@@ -18,9 +21,10 @@ const planetArray = planets.map(planet => {
     return newPlanetArray
 });
 
+// Use a filter method to check and log all elements to log all elements that includes() the letter "E"
 let ePlanet = planets.filter(planet => {
     let ePlanet = false;
-
+    // Conditional statement that'll check true to all elements that have the letter "E" in it.
     if (planet.includes("e")) {
         ePlanet = true;
         console.log(planet.toUpperCase())
